a8ecac05c7b338afac948a5790923f825e5a44e4
mohammad <shams.mohammad@yahoo.com>
2016-05-06 23:55:16 +0430

    تست 17 اردیبهشت

--- urlTest.py
+++ urlTest.py
@@ -1,6 +1,11 @@
 from urllib import request
+from urllib import response
+import sys
 
-x = request.urlopen("http://www.asriran.com")
-y = x.read()
+x = request.urlopen("http://www.tabnak.ir/")
+
+
+y = x.read().decode('cp850').encode(sys.stdout.encoding,'replace').decode(sys.stdout.encoding)
 
 print(y)
+
